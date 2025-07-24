e08e7f656b78ff948726fdf58e16e73752f9066f
Bulat Bogdanov <bulatmaster73@gmail.com>
2025-07-24 19:21:33 +0500

    updated update_trips.py

--- update_trips.py
+++ update_trips.py
@@ -12,46 +12,66 @@ def update_trips():
 
     request_url = f'{config.BX_WEBHOOK_URL}/{method}'
 
-    highest_id_rec = conn.execute('SELECT id FROM trips ORDER BY id DESC LIMIT 1').fetchone()
-    highest_id = highest_id_rec[0] if highest_id_rec else 0
+    last_modify_trip = conn.execute(
+        'SELECT date_modify FROM trips ORDER BY date_modify DESC LIMIT 1'
+    ).fetchone()
+
 
     offset = 0
 
     while True:
-        
-        r = requests.get(request_url, params={
+        params = {
             "entityTypeId": 176,  # Смарт процесс Командировки 
-            "FILTER[>ID]": highest_id,
             "start": offset
-        })
+        }
+        if last_modify_trip:
+            params["FILTER[>updatedTime]"] = last_modify_trip[0],
+        
+        r = requests.get(request_url, params=params)
         data = r.json()
 
-        to_insert = []
+        trips_db = {row[0] for row in conn.execute(
+            'SELECT id FROM trips'
+        ).fetchall()}
+
         for trip in data['result']['items']:
-            trip_id = trip['id']
+            trip_id = int(trip['id'])
             user_id = trip['assignedById']
             stage_id = trip['stageId']
             start_time = trip['begindate']
             end_time = trip['closedate']
+            date_modify = trip['updatedTime']
 
-            to_insert.append((trip_id, user_id, stage_id, start_time, end_time))
-        
-        if to_insert:
-            with conn:
-                conn.executemany(
+            if trip_id in trips_db:
+                conn.execute(
+                    """
+                    UPDATE trips SET
+                        user_id = ?, 
+                        stage_id = ?, 
+                        begin_time = ?, 
+                        end_time = ?, 
+                        date_modify = ?
+                    WHERE id = ?
+                    """, 
+                    (user_id, stage_id, start_time, end_time, date_modify, trip_id)
+                )
+            else:
+                conn.execute(
                     """
                     INSERT INTO trips 
-                    (id, user_id, stage_id, begin_time, end_time) 
-                    VALUES (?, ?, ?, ?, ?)
-                    ON CONFLICT DO NOTHING
-                    """, to_insert
+                    (id, user_id, stage_id, begin_time, end_time, date_modify) 
+                    VALUES (?, ?, ?, ?, ?, ?)
+                    """, 
+                    (trip_id, user_id, stage_id, start_time, end_time, date_modify)
                 )
+
         
         if data['total'] - offset <= 50:
             break 
 
         offset += 50
 
+    conn.commit()
 
 if __name__ == '__main__':
     update_trips()
